98749efac6148269aaa234c1fd940ade7fb18fdf
BanshwarTech <banshwaralekh6569@gmail.com>
2025-02-10 15:59:44 +0530

    second  Commit

--- Portfolio/resources/views/admin/dashboard.blade.php
+++ Portfolio/resources/views/admin/dashboard.blade.php
@@ -1,15 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.Includes.app')
+@section('page_title', 'Dashboard')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content -->
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <div class="row">
+                <div class="col-lg-8 mb-4 order-0">
+                    <div class="card">
+                        <div class="d-flex align-items-end row">
+                            <div class="col-sm-7">
+                                <div class="card-body">
+                                    <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
+                                    <p class="mb-4">
+                                        You have done <span class="fw-bold">72%</span> more sales today.
+                                        Check your new badge in
+                                        your profile.
+                                    </p>
 
-<body>
-    dfg
-</body>
+                                    <a href="javascript:;" class="btn btn-sm btn-outline-primary">View
+                                        Badges</a>
+                                </div>
+                            </div>
+                            <div class="col-sm-5 text-center text-sm-left">
+                                <div class="card-body pb-0 px-0 px-md-4">
+                                    <img src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}"
+                                        height="140" alt="View Badge User"
+                                        data-app-dark-img="illustrations/man-with-laptop-dark')}}"
+                                        data-app-light-img="illustrations/man-with-laptop-light')}}" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-</html>
+            </div>
+
+        </div>
+        <!-- / Content -->
+
+        <!-- Footer -->
+        <footer class="content-footer footer bg-footer-theme">
+            <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                <div class="mb-2 mb-md-0">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    , made with ❤️ by
+                    <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+                </div>
+                <div>
+                    <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                    <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More
+                        Themes</a>
+
+                    <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
+                        target="_blank" class="footer-link me-4">Documentation</a>
+
+                    <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
+                        class="footer-link me-4">Support</a>
+                </div>
+            </div>
+        </footer>
+        <!-- / Footer -->
+
+        <div class="content-backdrop fade"></div>
+    </div>
+@endsection
